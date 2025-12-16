266983cf409dd84a127f97cf5275e665b8833dd3
Chaithu23-hub <chaithravardhanreddy@gmail.com>
2025-12-16 12:23:33 +0530

    Added config class for shorturl entity to enable auditing

--- src/main/java/com/url/shortner/model/ShortUrl.java
+++ src/main/java/com/url/shortner/model/ShortUrl.java
@@ -7,11 +7,13 @@ import lombok.*;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.validator.constraints.URL;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
-
+@EntityListeners(AuditingEntityListener.class)
 @Table(
         name="short_urls",
         uniqueConstraints = {
@@ -48,7 +50,7 @@ public class ShortUrl {
     @Column(name = "created_at",updatable = false)
     private LocalDateTime createdAt;
 
-    @UpdateTimestamp
+    @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
